f63a09c1b12ac67f81c1bbd36bb9d751a98352bd
Okaramel <morell.mathilde@gmail.com>
2024-04-30 12:26:20 +0200

    Coyote Time

--- Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
+++ Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
@@ -4,6 +4,11 @@ public class HeroController : MonoBehaviour
 {
     [Header("Entity")]
     [SerializeField] private HeroEntity _entity;
+    private bool _entityWasTouchingGround = false;
+
+    [Header("Coyote Time")]
+    [SerializeField] private float _coyoteTimeDuration = 0.2f;
+    private float _coyoteTimeCountdown = -1f;
 
 
     [Header("Debug")]
@@ -20,6 +25,7 @@ public class HeroController : MonoBehaviour
         GUILayout.BeginVertical(GUI.skin.box);
         GUILayout.Label(gameObject.name);
         GUILayout.Label($"Jump Buffer Timer = {_jumpBufferTimer}");
+        GUILayout.Label($"CoyoteTime Countdown = {_coyoteTimeCountdown}");
         GUILayout.EndVertical();
     }
 
@@ -30,9 +36,17 @@ public class HeroController : MonoBehaviour
 
         _entity.SetMoveDirX(GetInputMoveX());
 
+        if(_EntityHasExitGround())
+        {
+            _ResetCoyoteTime();
+        } else
+        {
+            _UpdateCoyoteTime();
+        }
+
         if(_GetInputDownJump())
         {
-            if(_entity.IsTouchingGround && !_entity.IsJumping)
+            if(_entity.IsTouchingGround || !_entity.IsJumping && !_entity.IsJumping)
             {
                 _entity.JumpStart();
             } else
@@ -43,10 +57,12 @@ public class HeroController : MonoBehaviour
 
         if(IsJumperBufferActive())
         {
-            if(_entity.IsTouchingGround && !_entity.IsJumping) {
+            if((_entity.IsTouchingGround || _IsCoyoteTimeActive()) && !_entity.IsJumping) {
                 _entity.JumpStart();
             }
         }
+
+        _entityWasTouchingGround = _entity.IsTouchingGround;
 
         if (_entity.IsJumpImpulsing)
         {
@@ -120,5 +136,28 @@ public class HeroController : MonoBehaviour
     private void Start()
     {
         _CancelJumpBuffer();
+    }
+
+    private void _UpdateCoyoteTime()
+    {
+        if (!_IsCoyoteTimeActive()) return;
+        {
+            _coyoteTimeCountdown -= Time.deltaTime;
+        }
+    }
+
+    private void _ResetCoyoteTime()
+    {
+        _coyoteTimeCountdown = _coyoteTimeDuration;
+    }
+
+    private bool _IsCoyoteTimeActive()
+    {
+        return _coyoteTimeCountdown > 0f;
+    }
+
+    private bool _EntityHasExitGround()
+    {
+        return _entityWasTouchingGround && !_entity.IsTouchingGround;
     }
 }
